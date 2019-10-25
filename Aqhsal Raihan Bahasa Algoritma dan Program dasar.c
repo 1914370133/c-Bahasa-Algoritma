08e8b49d9db2bac8f4d5f51fb24056b4b29714f1
Muhammad Aqhsal Raihan Lubis <55613543+1914370133@users.noreply.github.com>
2019-10-25 18:14:08 +0700

    Add files via upload

---
+++ Aqhsal Raihan Bahasa Algoritma dan Program dasar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("                                    Muhammad Aqhsal Raihan\n");
+    printf("                                     NPM : 1914370133\n");
+    printf("                                  PRODI : Sistem Komputer\n");
+    printf("                                FAKULTAS : Sains Dan Teknologi\n");
+    int a=20;
+    int b=10;
+    int c;
+
+    c=a+b;
+    printf("baris 1 - nilai c adalah %d\n",c);
+    c=a-b;
+    printf("baris 2 - nilai c adalah %d\n",c);
+    c=a*b;
+    printf("baris 3 - nilai c adalah %d\n",c);
+    c=a/b;
+    printf("baris 4 - nilai c adalah %d\n",c);
+    c=a%b;
+    printf("baris 5 - nilai c adalah %d\n",c);
+    c=a++;
+    printf("baris 6 - nilai c adalah %d\n",c);
+    c=a--;
+    printf("baris 7 - nilai c adalah %d\n",c);
+    return 0;
+}
